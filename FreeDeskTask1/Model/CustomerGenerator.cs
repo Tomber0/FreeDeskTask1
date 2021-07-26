be82330d352a282492c307eaacb6217507bf603e
Tomber0 <aleseyzaicev@gmail.com>
2021-07-26 22:34:42 +0300

    added CustomerGenerator (not implemented)

---
+++ FreeDeskTask1/Model/CustomerGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeDeskTask1.Model
+{
+    class CustomerGenerator : ICustomerGenerator
+    {
+        public ICustomer GenerateCustomer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
