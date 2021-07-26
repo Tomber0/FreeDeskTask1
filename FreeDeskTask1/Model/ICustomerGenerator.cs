fd2a50fdba8e06d449f283fcde06caeb91d3c11d
Tomber0 <aleseyzaicev@gmail.com>
2021-07-26 22:32:56 +0300

    added IcustomerGenerator

---
+++ FreeDeskTask1/Model/ICustomerGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeDeskTask1.Model
+{
+    interface ICustomerGenerator
+    {
+        public ICustomer GenerateCustomer();
+    }
+}
