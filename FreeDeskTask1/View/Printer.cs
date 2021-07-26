2dd85600a40a8ba1144ad037652c935ab7c5e0b7
Tomber0 <aleseyzaicev@gmail.com>
2021-07-26 22:58:28 +0300

    Priner done
    console printer added

--- FreeDeskTask1/View/Printer.cs
+++ FreeDeskTask1/View/Printer.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace FreeDeskTask1.View
 {
-    class Printer
+    abstract class Printer
     {
+        public abstract void Print(string message);
+
     }
 }
